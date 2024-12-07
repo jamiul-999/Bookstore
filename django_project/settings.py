d6a020d0717e3df87991c63c41a3a760cef246fe
charlie <jhdhj999@gmail.com>
2024-12-07 18:27:35 +0600

    Image uploading feature added

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,7 +131,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
-
+MEDIA_url = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
